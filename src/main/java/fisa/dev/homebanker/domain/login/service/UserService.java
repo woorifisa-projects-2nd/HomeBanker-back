284d6a78a6df29fbe0f8c72f746c470d91d32a78
Cnythnk100 <cnythnk100@gmail.com>
2024-04-16 14:01:06 +0900

    #4 refactor: DTO Validation 추가, 코드 수정

--- src/main/java/fisa/dev/homebanker/domain/login/service/UserService.java
+++ src/main/java/fisa/dev/homebanker/domain/login/service/UserService.java
@@ -4,7 +4,6 @@ import fisa.dev.homebanker.domain.login.dto.UserRegisterDTO;
 import fisa.dev.homebanker.domain.login.entity.User;
 import fisa.dev.homebanker.domain.login.repository.UserRepository;
 import jakarta.transaction.Transactional;
-import java.util.Date;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -18,28 +17,18 @@ public class UserService {
   private final PasswordEncoder bCryptPasswordEncoder;
 
   public User register(UserRegisterDTO userRegisterDTO) {
-    String customerName = userRegisterDTO.getName();
-    Date customerBirth = userRegisterDTO.getBirth();
-    String customerPhone = userRegisterDTO.getPhone();
-    String customerAddress = userRegisterDTO.getAddress();
-    String customerLoginId = userRegisterDTO.getLoginId();
-    String customerLoginPw = userRegisterDTO.getLoginPw();
-    String customerRole = userRegisterDTO.getRole();
-    String customerIdentificationNum = userRegisterDTO.getIdentificationNum();
-    Date joinDate = userRegisterDTO.getJoinDate();
 
-    User user = new User();
-    user.setName(customerName);
-    user.setBirth(customerBirth);
-    user.setPhone(customerPhone);
-    user.setAddress(customerAddress);
-    user.setLoginId(customerLoginId);
-    user.setLoginPw(bCryptPasswordEncoder.encode(customerLoginPw));
-    user.setRole(customerRole);
-    user.setIdentificationNum(customerIdentificationNum);
-    user.setRecentLogin(null);
-    user.setJoinDate(joinDate);
-    userRepository.save(user);
-    return user;
+    return userRepository.save(User.builder()
+        .name(userRegisterDTO.getName())
+        .birth(userRegisterDTO.getBirth())
+        .phone(userRegisterDTO.getPhone())
+        .address(userRegisterDTO.getAddress())
+        .loginId(userRegisterDTO.getLoginId())
+        .loginPw(bCryptPasswordEncoder.encode(userRegisterDTO.getLoginPw()))
+        .role(userRegisterDTO.getRole())
+        .identificationNum(userRegisterDTO.getIdentificationNum())
+        .recentLogin(null) // 최근 로그인은 초기값으로 null 지정
+        .joinDate(userRegisterDTO.getJoinDate())
+        .build());
   }
 }
