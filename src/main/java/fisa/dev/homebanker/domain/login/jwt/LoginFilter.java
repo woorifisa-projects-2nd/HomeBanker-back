7048449a813e0609bf5108780f015d0c1e2bbaa1
Cnythnk100 <cnythnk100@gmail.com>
2024-04-17 14:55:02 +0900

    #4 fix: JWT 만료시간 재설정, 주석 수정

--- src/main/java/fisa/dev/homebanker/domain/login/jwt/LoginFilter.java
+++ src/main/java/fisa/dev/homebanker/domain/login/jwt/LoginFilter.java
@@ -62,7 +62,7 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     String role = auth.getAuthority();
 
     // JwtUtil에 token을 만들어달라고 전달
-    String token = jwtUtil.createJwt(loginId, role, 60 * 60 * 24L); // 1일
+    String token = jwtUtil.createJwt(loginId, role, 60 * 60 * 24 * 1000L); // 1일
     System.out.println("발급한 토큰 : " + token);
     response.addHeader("Authorization", "Bearer " + token);
   }
