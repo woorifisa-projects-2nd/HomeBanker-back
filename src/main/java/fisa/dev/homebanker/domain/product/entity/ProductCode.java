07d3a23730853031751a5ca3d578b8db877f4945
김경은 <kekim20@naver.com>
2024-04-24 14:01:23 +0900

    #35 chore: 기본 생성자 추가

--- src/main/java/fisa/dev/homebanker/domain/product/entity/ProductCode.java
+++ src/main/java/fisa/dev/homebanker/domain/product/entity/ProductCode.java
@@ -5,10 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ProductCode {
 
   @Id
