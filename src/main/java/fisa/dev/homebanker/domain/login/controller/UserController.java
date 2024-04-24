45f0ee1da68e9d3da76e2c8fc37393a93ea493cd
김경은 <kekim20@naver.com>
2024-04-24 12:19:21 +0900

    #35 chore: myPage 도메인 분리
    userController와 userService에서 myPage관련 부분 분리
    
    Co-Authored-By: Kyongryul Na <67616146+NaKyongRyul@users.noreply.github.com>

--- src/main/java/fisa/dev/homebanker/domain/login/controller/UserController.java
+++ src/main/java/fisa/dev/homebanker/domain/login/controller/UserController.java
@@ -3,16 +3,10 @@ package fisa.dev.homebanker.domain.login.controller;
 import fisa.dev.homebanker.domain.login.dto.UserRegisterDTO;
 import fisa.dev.homebanker.domain.login.entity.User;
 import fisa.dev.homebanker.domain.login.service.UserService;
-import fisa.dev.homebanker.domain.mypage.dto.MyPageProfileDTO;
 import fisa.dev.homebanker.domain.mypage.service.MyPageService;
-import fisa.dev.homebanker.domain.product.dto.SaleListDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,23 +20,5 @@ public class UserController {
   public User register(@RequestBody UserRegisterDTO userRegisterDTO) {
     User user = userService.register(userRegisterDTO);
     return user;
-  }
-
-  @GetMapping("/api/mypage/profile")
-  public ResponseEntity<MyPageProfileDTO> readMyPage() {
-    return ResponseEntity.ok(myPageService.readMyPage());
-  }
-
-  @PostMapping("/api/mypage/profile")
-  public ResponseEntity<MyPageProfileDTO> updateMyPage(
-      @RequestBody @Validated MyPageProfileDTO myPageProfileDTO) {
-    return ResponseEntity.ok(myPageService.updateMyPage(myPageProfileDTO));
-  }
-
-  @GetMapping("/api/mypage/sale")
-  public ResponseEntity<SaleListDTO> findAllSales(
-      @RequestParam Integer size, @RequestParam(defaultValue = "0") Integer page
-  ) {
-    return ResponseEntity.ok(myPageService.findAllSales(size, page));
   }
 }
