9a36f7896615c4024fc8b38f799894a8fc79de80
Cnythnk100 <cnythnk100@gmail.com>
2024-05-01 16:41:06 +0900

    #41 feat: CORS 설정 및 API 통신 테스트 성공

--- src/main/java/fisa/dev/homebanker/domain/video/service/IdentifyUserService.java
+++ src/main/java/fisa/dev/homebanker/domain/video/service/IdentifyUserService.java
@@ -2,13 +2,11 @@ package fisa.dev.homebanker.domain.video.service;
 
 import fisa.dev.homebanker.domain.board.dto.IdCardContentDTO;
 import jakarta.annotation.PostConstruct;
-import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
-@Transactional
 public class IdentifyUserService {
 
   private WebClient webClient;
@@ -26,6 +24,8 @@ public class IdentifyUserService {
           .retrieve()
           .bodyToMono(String.class)
           .block(); // 동기적으로 응답을 기다리고 결과를 반환
+
+      System.out.println("신분증 검증 성공");
 
       // 응답 확인 후 처리
       return "true".equals(response);
